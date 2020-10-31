62778ca6950b48c9f5111bcedca2ce4476bad567
Aayush Patil <aayushaptil558321@gmail.com>
2020-10-31 15:38:52 +0530

    Added authentication and forgot password page/

--- CRM/urls.py
+++ CRM/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import HttpResponse
 
+def home (request):
+    return HttpResponse('This is homepage')
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
-]
+    path('', home, name="home")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
-from .views import login_request, signup_request
-from django.urls import path
+from .views import login_request, signup_request, logout_request, password_reset_request
+from django.urls import path, include, reverse_lazy
+from django.contrib.auth import views as auth_views
+from django.shortcuts import HttpResponseRedirect
 
 
 app_name = 'accounts'
@@ -7,4 +9,9 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', login_request, name="login"),
     path('signup/', signup_request, name="signup"),
+    path("password-reset/", password_reset_request, name="password_reset"),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html", success_url= reverse_lazy('accounts:password_reset_complete')), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'), 
+    
 ]
